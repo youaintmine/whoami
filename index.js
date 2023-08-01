3aea61298699bf0478f24161be19e95ed668e6de
youaintmine <ahmbarishsaikia103@gmail.com>
2023-08-01 12:56:33 +0530

    dep-1

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+//ip enable
+app.enable('trust proxy')
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -19,10 +21,37 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
+// "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+
+
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', (req, res) =>{
+
+  //Ip Get
+  // console.log(req.ip);
+  //User-Agent
+  let len = req.rawHeaders.length;
+  let softwareObj = {};
+  for(let i = 0; i<len; i+=2){
+    softwareObj[`${req.rawHeaders[i]}`] = req.rawHeaders[i+1];
+  }
+  // console.log(software['User-Agent']);
+  // console.log(software['Accept-Language']);
+
+  
+  res.send({
+    ipaddress : req.ip,
+    language : softwareObj['Accept-Language'],
+    software : softwareObj['User-Agent']
+  })
+
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
